f3efc7ce47a026331b7865c5220666b01568a67f
dimasrhesky270206 <dimashandaresky@gmail.com>
2024-10-31 09:00:22 +0700

    Rata nilai

---
+++ ArrayNilai06.java
@@ -0,0 +1,13 @@
+public class ArrayNilai06 {
+    public static void main(String[] args) {
+        int[] nilaiAkhir = new int [10];
+    for (int i = 0; i < nilaiAkhir.length; i++) {
+        System.out.print ("Masukkan nilai akhir ke- "+i+": ");
+        if (nilaiAkhir [i] > 70) {
+            System.out.println("Mahasiswa ke-"+i+"lulus!");
+        }
+        
+     }  
+   }
+     }
+    
